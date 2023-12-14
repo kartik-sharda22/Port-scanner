be810e5f84cea5e4c1d7290bbb021c727adca219
kartik-sharda22 <kartiksharda51@gmail.com>
2023-12-14 15:27:48 +0530

    Add thread

--- port_scanner.py
+++ port_scanner.py
@@ -1,17 +1,16 @@
 #!/bin/python3
-
 import sys
 import socket
 from datetime import datetime
+import threading
 
-#Define our target
+# Define our target
 if len(sys.argv) == 2:
-	target = socket.gethostbyname(sys.argv[1]) #Translate hostname to IPv4
+    target = socket.gethostbyname(sys.argv[1])      #Translate hostname to IPv4
 else:
-	print("Invalid amount of arguments")
-	print("Syntax: python3 port_scanner.py <ip>")
-	
-#Add ASCII art
+    print("Invalid amount of arguments\nSyntax: python3 port_scanner.py <ip>")
+
+# Add ASCII art
 print('''
   _____   ____  _____ _______    _____  _____          _   _ _   _ ______ _____   
  |  __ \ / __ \|  __ \__   __|  / ____|/ ____|   /\   | \ | | \ | |  ____|  __ \  
@@ -21,30 +20,48 @@ print('''
  |_|     \____/|_|  \_\ |_|    |_____/ \_____/_/    \_\_| \_|_| \_|______|_|  \_\ 
                                                                                   
                                                                                   ''')
-print("Target IP: " + target)
-#Add a pretty banner
+
+print("Target IP:" + target)
+print("\n")
+
+# Add a pretty Banner
 print("-" * 50)
-print("Scanning target: "+target)
-print("Time started: "+str(datetime.now()))
+print("Scanning target: " + target)
+print("Time started: " + str(datetime.now()))
 print("-" * 50)
 
+def scan_port(port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target,port))
+        if result == 0:
+            print(f"Port {port} is open.")
+        s.close()
+
+    except KeyboardInterrupt:
+        print("\nExiting program.")
+        sys.exit()
+
+    except socket.gaierror:
+        print("Hostname could not be resolved.")
+        sys.exit()
+
+    except socket.error:
+        print("Could no connect to the server.")
+        sys.exit()
+
 try:
-	for port in range(1,999):
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		socket.setdefaulttimeout(1)
-		result = s.connect_ex((target,port))
-		if result == 0:
-			print(f"Port {port} is open.")
-		s.close()
-		
+    threads = []
+
+    for port in range(1,65535):
+        thread = threading.Thread(target=scan_port, args=(port,))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
 except KeyboardInterrupt:
-	print("\nExiting program.")
-	sys.exit()
-	
-except socket.gaierror:
-	print("Hostname could not be resolved.")
-	sys.exit()
-	
-except socket.error:
-	print("Could not connect to server.")
-	sys.exit()
+    print("\nExiting program.")
+    sys.exit()
